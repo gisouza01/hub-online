789090e1ab51cb274b99578d9a86122883813dd6
gisouza01 <gisouza@sempreceub.com>
2025-09-27 14:33:10 -0300

    modificações

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("botao_1").onclick = function()
+        {alert("Login com sucesso!");};
+
+
+document.getElementById("botao_2").onclick = function()
+{alert("Login com sucesso!");};
